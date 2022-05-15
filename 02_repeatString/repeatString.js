294798125f992c1a2694ef07e45493b550143aba
Code4Daze <jevon.fillette@gmail.com>
2022-05-15 19:08:29 -0400

    Complete js exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+  let answer = "";
+  for (i = 0; i < num; i++) {
+    answer += word;
+  }
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    return answer;
+  }
 };
 
 // Do not edit below this line
