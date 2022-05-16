19da74aaf4c8db37cce0cfec0e6cafc756ed38c6
Code4Daze <jevon.fillette@gmail.com>
2022-05-15 22:05:18 -0400

    Complete js exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+  let finalSum = 0;
+  let largerNum;
+  let smallerNum;
 
+  if (startNum < 0 || endNum < 0) {
+    return "ERROR";
+  } else if (typeof startNum !== "number" || typeof endNum !== "number") {
+    return "ERROR";
+  } else {
+    if (endNum >= startNum) {
+      largerNum = endNum;
+      smallerNum = startNum;
+    } else {
+      largerNum = startNum;
+      smallerNum = endNum;
+    }
+    for (i = smallerNum; i <= largerNum; i++) {
+      finalSum += i;
+    }
+    return finalSum;
+  }
 };
 
 // Do not edit below this line
