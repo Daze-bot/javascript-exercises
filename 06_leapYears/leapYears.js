5d55a57f2677b88cbd3872225758c20dbc8bab97
Code4Daze <jevon.fillette@gmail.com>
2022-05-15 22:24:01 -0400

    Complete js exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(calendarYear) {
+  if (calendarYear % 100 === 0 && calendarYear % 400 !== 0) {
+    return false;
+  } else {
+    if (calendarYear % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 // Do not edit below this line
