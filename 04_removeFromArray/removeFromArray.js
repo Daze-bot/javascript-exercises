d591cf2f58d0b359fb09b8ff35a93dfdb80f3729
Code4Daze <jevon.fillette@gmail.com>
2022-05-15 21:23:20 -0400

    Complete js exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+  let mainArray = args[0];
+  let remove = args.slice(1);
+  let newArray = [];
+  mainArray.forEach((arrayItem) => {
+    if (!remove.includes(arrayItem)) {
+      newArray.push(arrayItem);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
