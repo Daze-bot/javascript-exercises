a9555b4c7c89fb76bf9c56ccd2171fca39d8e44f
Code4Daze <jevon.fillette@gmail.com>
2022-05-26 22:12:53 -0400

    Complete js exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayList) {
+	let arraySum = arrayList.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+  }, 0);
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayList) {
+  let arrayMultiply = arrayList.reduce((previousValue, currentValue) => {
+    return previousValue * currentValue;
+  });
+  return arrayMultiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) return undefined;
+  if (num === 0) return 1;
+  let x = num - 1;
+  while (x > 1) {
+    num *= x;
+    x--;
+  }
+  return num;
 };
 
 // Do not edit below this line
