4b5afc94f8408ba6fce6748591b0360b242ae451
Code4Daze <jevon.fillette@gmail.com>
2022-05-26 23:09:22 -0400

    Complete js exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let result = str.toLowerCase();
+  result = result.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "");
+  let splitString = result.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  if (joinArray === result) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
