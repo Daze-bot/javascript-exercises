f64b6ea93f1942091a129cc8d4db9179ebe40b20
Code4Daze <jevon.fillette@gmail.com>
2022-05-27 01:34:17 -0400

    Complete js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,46 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+const findTheOldest = function(array) {
+  let oldest = array.sort((a, b) => {
+    return (getAge(a.yearOfBirth, a.yearOfDeath)) > (getAge(b.yearOfBirth, b.yearOfDeath)) ? -1 : 1
+  });
+  let first = oldest[0];
+
+  let alive = array.some(person => {
+    if (!person.yearOfDeath) {
+      return true;
+    }
+  });
+
+  if (alive) {
+    return first;
+  } else {
+    return "All Dead";
+  }
 };
 
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
+
+console.log(findTheOldest(people));
 // Do not edit below this line
-module.exports = findTheOldest;
+//module.exports = findTheOldest;
