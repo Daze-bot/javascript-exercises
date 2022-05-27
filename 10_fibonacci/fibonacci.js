6cad21c53d489d1d20392aaa234a9c1236b04823
Code4Daze <jevon.fillette@gmail.com>
2022-05-26 23:54:51 -0400

    Complete js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  let inputNumber = +input;
+  if (inputNumber <= 0) {
+    return "OOPS";
+  }
 
+  let fib = [0, 1];
+
+  for (let i = 2; i <= inputNumber; i++) {
+    fib[i] = fib[i -2] + fib[i - 1];
+  }
+
+  return fib[inputNumber];
 };
 
 // Do not edit below this line
